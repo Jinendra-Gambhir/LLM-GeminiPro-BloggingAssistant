274bfd1a21979b88440242d498dd3e7b577ebb7c
Jinendra-Gambhir <30826178+Jinendra-Gambhir@users.noreply.github.com>
2024-06-14 02:37:28 -0700

    Delete apikey.py

--- apikey.py
+++
@@ -1,4 +0,0 @@
-openai_api_key="sk-proj-1bCsIjkT3NRHx7h3KJjCT3BlbkFJyLsdn7aeamZJ0O3LxIkW"
-google_gemini_api_key="AIzaSyAEMyPC-Tkhj5zEHurqtLeYAhKCimbWnTs"
-
-#https://aistudio.google.com/app/prompts/1Toj85FoaQIzcox_tOAjGlchuC8LolpTP
