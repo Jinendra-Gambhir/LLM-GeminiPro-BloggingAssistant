cab60ce08fa209f8782e180d5ad68f91b6e593fa
Jinendra-Gambhir <30826178+Jinendra-Gambhir@users.noreply.github.com>
2024-06-14 02:25:34 -0700

    Add files via upload

---
+++ apikey.py
@@ -0,0 +1,4 @@
+openai_api_key="sk-proj-1bCsIjkT3NRHx7h3KJjCT3BlbkFJyLsdn7aeamZJ0O3LxIkW"
+google_gemini_api_key="AIzaSyAEMyPC-Tkhj5zEHurqtLeYAhKCimbWnTs"
+
+#https://aistudio.google.com/app/prompts/1Toj85FoaQIzcox_tOAjGlchuC8LolpTP
